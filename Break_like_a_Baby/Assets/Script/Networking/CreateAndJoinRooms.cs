1fe944a56a853d0e09c37e6039e42c88b664610e
Arnav Arora <aa10027@nyu.edu>
2025-02-09 14:57:10 -0500

    Camera Movement

--- Break_like_a_Baby/Assets/Script/Networking/CreateAndJoinRooms.cs
+++ Break_like_a_Baby/Assets/Script/Networking/CreateAndJoinRooms.cs
@@ -23,7 +23,7 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
     }
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("Arnav_NetworkTest");
+        PhotonNetwork.LoadLevel("Arnav_CameraTest");
     }
     // Update is called once per frame
     void Update()
