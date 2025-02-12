7c1cc81ed6e54392736db75d58977c2a2c728c70
Arnav Arora <aa10027@nyu.edu>
2025-02-12 13:17:46 -0500

    Multiplayer Break

--- Break_like_a_Baby/Assets/Script/Player/PlayerBreak.cs
+++ Break_like_a_Baby/Assets/Script/Player/PlayerBreak.cs
@@ -1,12 +1,16 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using Photon.Pun;
 
-public class PlayerBreak : MonoBehaviour
+public class PlayerBreak : MonoBehaviourPunCallbacks
 {
     //Rigidbody rb;
     //[SerializeField] public float moveSpeed = 5f;
     //[SerializeField] public float rotationSpeed = 10f;
     //[SerializeField] private Vector3 moveDirection;
+
+    public bool inRange = false;
+    GameObject breakable;
     private void Start()
     {
         //rb = this.GetComponent<Rigidbody>();
@@ -23,10 +27,19 @@ public class PlayerBreak : MonoBehaviour
         //    Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
         //    transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, Time.deltaTime * rotationSpeed);
         //}
+        if(inRange && Input.GetKeyDown(KeyCode.Space) && photonView.IsMine)
+        {
+           
+                Debug.Log("pressed space");
+                breakable.GetComponent<BreakableObject>().takeDamage();
+
+       
+        }
+
     }
 
     void FixedUpdate()
-        {
+     {
             //// Move the baby
             //rb.linearVelocity = moveDirection * moveSpeed + new Vector3(0, rb.linearVelocity.y, 0);
         }
@@ -34,17 +47,41 @@ public class PlayerBreak : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Initial: " + collision.gameObject.name);
+
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Breakable" && Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("pressed space");
-            other.gameObject.GetComponent<BreakableObject>().takeDamage();
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Breakable" )
+    //    {
 
+    //        breakable = other.gameObject;
+    //        inRange = true;
+                
+    //    }
+    //}
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject == breakable)
+    //    {
+    //        inRange = false;
+
+    //    }
+    //}
+
+    public void breakableInRange(bool isInRange , GameObject breakableObj)
+    {
+        if (isInRange)
+        {
+            Debug.Log(gameObject.name + isInRange);
+            inRange = true;
+            breakable = breakableObj;
+        }
+        if( !isInRange && breakable == breakableObj)
+        {
+            Debug.Log(gameObject.name + isInRange);
+            inRange = false;
         }
     }
-
 
 }
