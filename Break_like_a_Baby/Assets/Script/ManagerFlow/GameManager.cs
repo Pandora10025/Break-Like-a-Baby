ee5ce20002365463309c97287a4ce127d01760e4
Arnav Arora <aa10027@nyu.edu>
2025-02-26 13:13:32 -0500

    Game Manager

--- Break_like_a_Baby/Assets/Script/ManagerFlow/GameManager.cs
+++ Break_like_a_Baby/Assets/Script/ManagerFlow/GameManager.cs
@@ -1,26 +1,55 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set; }
+
+    bool gameOver=true;
+    public string timerUItext;
+    [SerializeField] float totalTime;
+    [SerializeField] bool gameStarted;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         instance = this;
 
-
+        gameStarted = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int minutes = Mathf.FloorToInt(totalTime / 60);
+        int seconds = Mathf.FloorToInt(totalTime % 60);
+        if (gameStarted)
+        {
+            totalTime -= Time.deltaTime;
+            
+            if (totalTime <= 0)
+            {
+                totalTime = 0;
+                GameOver(true);
+
+            }
+           
+        }
+
+        timerUItext = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+
     }
 
     public void GameOver(bool timer)
     {
-        
+        gameOver = true;
     }
+
+
+
+    
+
 
 
 }
