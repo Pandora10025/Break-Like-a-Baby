394087feae55ddc3878f562c74ae7fade049ef9c
Pandora10025 <98975655+Pandora10025@users.noreply.github.com>
2025-04-16 11:11:59 -0400

    Setting Up Icon UI Retrieval

---
+++ Break_like_a_Baby/Assets/Script/UI/UI_Icon_Scriptable_Objects.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UI_Icon", menuName = "Scriptable Objects/UI_Icon",order = 1)]
+public class UI_Icon_Scriptable_Objects : ScriptableObject
+{
+    public string  iconName;
+
+    public SpriteRenderer spriteRenderer;
+
+}
