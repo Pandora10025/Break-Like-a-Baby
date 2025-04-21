5dfc146b74597f88beb72f1e5d5aae2947fef1db
oirbeboq <revamqq@gmail.com>
2025-04-21 13:43:36 -0400

    CutScene update

--- Break_like_a_Baby/Assets/Script/ManagerFlow/NextScene.cs
+++ Break_like_a_Baby/Assets/Script/ManagerFlow/NextScene.cs
@@ -4,18 +4,7 @@ using UnityEngine.SceneManagement;
 public class NextScene : MonoBehaviour
 {
     [SerializeField] private string nextScene;
-    
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+ 
     public void loadNextScene()
     {
         SceneManager.LoadScene(nextScene);
