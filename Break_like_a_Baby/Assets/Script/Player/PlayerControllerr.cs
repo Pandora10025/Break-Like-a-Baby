db11c6caed0b2a3793c87bccd4cedb81b7ea9ddb
SnowyOchole <annabelleochotta@gmail.com>
2025-05-03 00:19:19 -0400

    um

--- Break_like_a_Baby/Assets/Script/Player/PlayerControllerr.cs
+++ Break_like_a_Baby/Assets/Script/Player/PlayerControllerr.cs
@@ -86,6 +86,8 @@ public class PlayerControllerr : MonoBehaviourPunCallbacks, IInRoomCallbacks
     public Slider sprintBar;
     public float minEmiss=15f;
     bool clDown = true;
+
+    
     void Awake()
     {
         inputActions = new PlayerControls();
